68358aed64978ec5a2f98f19105ce37801a0f0b6
Florin Alexandrescu <florin.alexandrescu@mapquest.com>
2016-08-23 15:43:28 -0600

    Make test load ireal file synchronously.

--- spec/parser-spec.js
+++ spec/parser-spec.js
@@ -3,35 +3,53 @@
 const fs = require('fs');
 const iRealReader = require('../index');
 
-fs.readFile("spec/Tester.html", "utf8", function(err, data){
-  if(err) console.log(err);
-  var p = new iRealReader(data);
-//  console.log('tester song: ', p.songs[0].music.raw);
-  var measures = p.songs[0].music.measures;
+const iRealData = fs.readFileSync("spec/Tester.html", 'utf8');
+const songData = new iRealReader(iRealData);
 
-  describe('iRealPro parser', function(){
+const testSong = songData.songs[0].music.measures;
+const theBreezeAndI = songData.songs[3].music.measures;
 
-    it('should have the right number of measures', function(){
-      expect(measures.length).toEqual(11);
-    });
+console.log('testSong raw', songData.songs[0].music.raw);
+console.log('testSong measures', testSong);
 
-    it('every chord returned should start with A, B, C, D, E, F, or G', function(){
-      expect(measures.every(chord => (/^[a-g]/i).test(chord))).toEqual(true);
-    });
+describe('iRealPro parser', function(){
 
-    it('should fill in double repeats', function(){
-      expect(measures[2]).toEqual(['C'])
-      expect(measures[3]).toEqual(['C#'])
-    });
+  it('should have the right number of measures', function(){
+    expect(testSong.length).toEqual(9);
+  });
 
-    it('should fill in invisible slash chords', function(){
-      expect(measures[7]).toEqual(['E/D', 'E/C', 'E/B'])
-    });
+  it('every chord returned should start with A, B, C, D, E, F, or G', function(){
+    expect(testSong.every(chord => (/^[a-g]/i).test(chord))).toEqual(true);
+  });
 
-    it('should fill in single repeats', function(){
-      expect(measures[9]).toEqual(['Ab'])
-    });
+  it('should fill in double repeats', function(){
+    expect(testSong[2]).toEqual(['C'])
+    expect(testSong[3]).toEqual(['C#'])
+  });
+
+  it('should fill in invisible slash chords', function(){
+    expect(testSong[7]).toEqual(['E/D', 'E/C', 'E/B'])
+  });
+
+  it('should fill in single repeats', function(){
+    expect(testSong[9][0]).toEqual('Ab')
+  });
+
+  it('should handle Kcl repeats correctly', function(){
+    expect(theBreezeAndI[2][0]).toEqual('Eb6');
+    expect(theBreezeAndI[3][0]).toEqual('Eb6');
+    expect(theBreezeAndI[5][0]).toEqual('Db-7');
+  });
+
+  it('should handle Kcl repeats correctly', function(){
+    expect(theBreezeAndI[2][0]).toEqual('Eb6');
+    expect(theBreezeAndI[3][0]).toEqual('Eb6');
+    expect(theBreezeAndI[5][0]).toEqual('Db-7');
+  });
+
+  it('should handle slash chords correctly', function(){
+    expect(theBreezeAndI[10][0]).toEqual('F-7/Bb');
   });
 
 
-});
+})
